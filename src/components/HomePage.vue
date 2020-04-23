d67bb0adbd5021113560a9f56558992189990097
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-04-23 17:09:11 -0400

    first push

---
+++ src/components/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+     <div class="jumbotron jumbotron-fluid">
+         <div class="container">
+             <h1 class="display-3">Horizon Library</h1>
+             <p class="lead">Search and Explore</p>
+             <hr class="my-2">
+             <p>Choose a category and search through hundreds of avaliable options. Feel free to click one you like and get additional information.</p>
+             <p>Horizons Library is a great tool to use through your time playing to find and learn about the game</p>
+         </div>
+     </div>
+     <div class="container">
+         <div class="row home-row">
+             <div class="col-sm-6 home-col">
+                 <h1>Fish</h1>
+             </div>
+             <div class="col-sm-6 home-col">
+                 <h1>Villagers</h1>
+             </div>
+         </div>
+         <div class="row home-row">
+             <div class="col-sm-6 home-col">
+                <h1>Fossils</h1>                  
+             </div>
+             <div class="col-sm-6 home-col">
+                <h1>Bugs</h1>
+             </div>
+         </div>
+     </div>
+    </div>
+</template>
+ 
+          
+        
+<style>
+
+.home-row {
+    margin-bottom: 5%;
+}
+
+.home-col:hover{
+    background-color:  	#ff9391;
+    border-radius: 5px;
+}
+
+</style>
+
