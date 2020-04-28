5cf45e8f3f5090d54357c156e5b774bc85842847
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-04-28 18:05:56 -0400

    currentFish working, going to get images with axios as well

--- src/components/FishComps/FishShow.vue
+++ src/components/FishComps/FishShow.vue
@@ -1,7 +1,33 @@
 <template>
     <div>
-    {{currentFish.id}}
-    {{currentFish.price}}
+        <b-card
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 40rem;"
+            class="mb-2 mt-2 mx-auto"
+        >
+            <b-card-title>{{currentFish.name['name-en']}}</b-card-title>
+            <b-card-text>{{currentFish['catch-phrase']}}</b-card-text>
+            <b-row>
+                <b-col class='mb-5 mt-2'> 
+                    <b-card-text>Avaliability</b-card-text>
+                    <b-card-text>Location: {{currentFish.availability.location}}</b-card-text>
+                    <b-card-text>Rarity: {{currentFish.availability.rarity}}</b-card-text>
+                    <b-card-text>Rarity: {{currentFish.availability.rarity}}</b-card-text>
+                    <b-card-text>All day?: {{currentFish.availability.isAllDay}}</b-card-text>
+                    <b-card-text>All year?: {{currentFish.availability.isAllYear}}</b-card-text>
+                    <b-card-text>Through: {{currentFish.availability['month-northern']}} and {{currentFish.availability['month-southern']}}</b-card-text>
+                </b-col>
+                <b-col class='mb-5 mt-2'> 
+                    <b-card-text>Price</b-card-text>
+                    <b-card-text>{{currentFish.price}} bells</b-card-text>
+                </b-col>
+            </b-row>
+                                <b-card-text>Musuem Description</b-card-text>
+                    <b-card-text>{{currentFish['museum-phrase']}}</b-card-text>
+        </b-card>
     </div>
     
 </template>
--- src/components/NavPage.vue
+++ src/components/NavPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-navbar type='dark'>
+        <b-navbar type='dark' variant='dark'>
             <b-navbar-nav>
                 <b-nav-item href="#"><router-link to="/">Home</router-link></b-nav-item>
                 <b-nav-item href="#">About</b-nav-item>
