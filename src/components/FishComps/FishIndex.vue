6ffb641495e24e664b0fa618e90cc0947b03964f
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-04-29 18:22:13 -0400

    comparision of fish and villagers for loading

--- src/components/FishComps/FishIndex.vue
+++ src/components/FishComps/FishIndex.vue
@@ -1,19 +1,14 @@
 <template>
 <div>
-    <div v-if="loading">
-        <b-spinner variant="primary" style="width: 3rem; height: 3rem;" label="Large Spinner"></b-spinner>
-    </div>
-    <div v-else>
-        <h1>Fish</h1>
-        <p>Click a fish and get all its additional details</p>
-        <div v-for="f in fish" v-bind:key='f.id' class="card">
-            <b-card text-variant="primary">
-                 <router-link :to="'/currentFish/' + f.id" >
-                 <b-card-title>{{f.name['name-en']}}</b-card-title>
-                 <b-card-text>${{f.price}}</b-card-text>
-                </router-link>
+    <h1>Fish</h1>
+    <p>Click a fish and get all its additional details</p>
+    <div v-for="f in fish" v-bind:key='f.id' class="card">
+        <b-card text-variant="primary">
+            <router-link :to="'/currentFish/' + f.id" >
+            <b-card-title>{{f.name['name-en']}}</b-card-title>
+            <b-card-text>${{f.price}}</b-card-text>
+            </router-link>
             </b-card>
-        </div>
     </div>
 </div>
 </template>
@@ -26,17 +21,14 @@ export default {
     data() {
         return{
             fish: [],
-            loading: false,
         }
     },
 
     created(){
-        this.loading = true 
         axios.get('http://acnhapi.com/fish/')
         .then(fish => {
             this.fish = fish.data
         })
-        .finally(() => (this.loading = false))
     },
 }
 </script>
