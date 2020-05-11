30621b6d811418433196fd638980ca740e8f8156
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-05-10 21:41:43 -0400

    about page layout created, about the site card done, now going to do technical about section

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <p>hi</p>
+        <b-container>
+            <b-row>
+                <b-col>
+                    <b-card>
+                        <b-card-text>Horizons library is quick and easily acessible information regarding everything Animal Crossing New Horizons. You can currently switch between Fish and Villagers (Bugs and Fossils coming soon) and get their image and detailed information. This site is also free to use and does not require and type of login. Just c ome visit and start gathering your information. Whether you're curious on how much your next big catch will be, or what wondering what Antonois personality is going to be when you meet him, it's simple to find.</b-card-text>
+                        <b-card-text>Another very imporant aspect of the site is how it was made. Below is just some information on things like what tools where used, where all this data is coming from and more.</b-card-text>
+                    </b-card>
+                </b-col>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
