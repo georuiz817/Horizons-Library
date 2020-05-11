b70ed7ae13bc974949661e2036adf20b0f1f32ca
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-05-10 22:07:47 -0400

    1st card of technical about done talking about vue next is bootstrap vue'

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,15 +1,28 @@
 <template>
     <div>
-        <b-container>
-            <b-row>
-                <b-col>
-                    <b-card>
-                        <b-card-text>Horizons library is quick and easily acessible information regarding everything Animal Crossing New Horizons. You can currently switch between Fish and Villagers (Bugs and Fossils coming soon) and get their image and detailed information. This site is also free to use and does not require and type of login. Just c ome visit and start gathering your information. Whether you're curious on how much your next big catch will be, or what wondering what Antonois personality is going to be when you meet him, it's simple to find.</b-card-text>
-                        <b-card-text>Another very imporant aspect of the site is how it was made. Below is just some information on things like what tools where used, where all this data is coming from and more.</b-card-text>
-                    </b-card>
+        <b-card align='left'>
+            <b-card-text>Horizons library is quick and easily acessible information regarding everything Animal Crossing New Horizons. You can currently switch between Fish and Villagers (Bugs and Fossils coming soon) and get their image and detailed information. This site is also free to use and does not require and type of login. Just c ome visit and start gathering your information. Whether you're curious on how much your next big catch will be, or what wondering what Antonois personality is going to be when you meet him, it's simple to find.</b-card-text>
+            <b-card-text>Another very imporant aspect of the site is how it was made. Below is just some information on things like what tools where used, where all this data is coming from and more.</b-card-text>
+        </b-card>
+
+        <b-card title='Vue.js'>
+            <b-container>
+                        <b-row>
+                <b-col col sm='8'>
+            
+            <b-card-text align='left'>
+             Horizons Library is a front end website made with Vue.js and deployed on the web PaSS Heroku. Having built websites with React and vanilla HTML/CSS, I was curious to try the newer and different framework. It provide a lightweight and easy to use template, as well as a beautiful UX friendly Vue Bootstrap to make your site mobile responsive and aesthitcally pleasing.
+            </b-card-text>
                 </b-col>
-            </b-row>
-        </b-container>
+                <b-col>
+                    <img src='../assets/logo.png'/>
+                </b-col>
+                </b-row>
+            </b-container>
+
+        </b-card>
+
+
     </div>
 </template>
 
