b42515d7bec0072970c72bc9785a0705f096d7b8
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-05-24 20:34:21 -0400

    bugs and fossils with routes updated to navbar

--- src/components/NavPage.vue
+++ src/components/NavPage.vue
@@ -8,6 +8,8 @@
                 <b-nav-item-dropdown text="Categories" right>
                     <b-dropdown-item href="#"><router-link class='r-link' to="/fish">Fish</router-link></b-dropdown-item>
                     <b-dropdown-item href='#'><router-link class='r-link' to='/villagers'>Villagers</router-link></b-dropdown-item>
+                    <b-dropdown-item href='#'><router-link class='r-link' to='/fossils'>Fossils</router-link></b-dropdown-item>
+                    <b-dropdown-item href='#'><router-link class='r-link' to='/bugs'>Bugs</router-link></b-dropdown-item>
                 </b-nav-item-dropdown>
             </b-navbar-nav>
         </b-navbar>
