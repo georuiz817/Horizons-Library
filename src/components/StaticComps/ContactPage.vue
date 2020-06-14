14562944a16608856e1ebf11dcbb7fd2c16380d3
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-06-13 23:12:57 -0400

    contact page done

--- src/components/StaticComps/ContactPage.vue
+++ src/components/StaticComps/ContactPage.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <b-card>
-                        <p>Email any questions,comments, or concerns to <a href="mailto:ruiz.fullstack@gmail.com">ruiz.fullstack@gmail.com</a></p>
-                <p>The websites GitHub repo can be viewed <a href='https://github.com/georuiz817/Horizons-Library' target="_blank">here</a></p>
-        </b-card>
+        <b-container>
+        <b-jumbotron align='left' bg-variant="light" class='mt-3 mx-auto'  header="Contact" lead="">
+        <p>Email any questions,comments, or concerns to <a href="mailto:ruiz.fullstack@gmail.com">ruiz.fullstack@gmail.com</a></p>
+        <p>To view my other work you can visit my GitHub  <a href='https://github.com/georuiz817/Horizons-Library' target="_blank">here</a> </p>
+        <p>This websites GitHub repo can be viewed <a href='https://github.com/georuiz817/Horizons-Library' target="_blank">here</a></p>
+        </b-jumbotron>
+        </b-container>
     </div>
 </template>
